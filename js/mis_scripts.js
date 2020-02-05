f09de569cd6a0eaf676fcf3ae4ff0605244b4dcc
Marco Ruvalcaba <desarrollo@oversistemas.com>
2020-02-05 14:25:00 -0600

    Creando el Slide de imagenes con Swipe

--- js/mis_scripts.js
+++ js/mis_scripts.js
@@ -1,7 +1,7 @@
 $(function() {
 
   /*==============================================================================================
-  FUNCIONES PARA EL MENU PRINCIPAL
+  1. FUNCIONES PARA EL MENU PRINCIPAL
   ==============================================================================================*/
 
   /*Insertar y quitar la clase ".icono-cerrar" al boton del menu*/
@@ -18,6 +18,37 @@ $(function() {
 
     /* 2) Contraemos el menú*/
     $('#menu-navegacion .navbar-collapse').collapse('hide');
+  });
+
+  /*==============================================================================================
+  2. SCRIPT PARA SWIPER
+  ==============================================================================================*/
+  var swiper = new Swiper('.swiper-container', {
+    /*botones de navegación*/
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    /*Botones de paginación*/
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+      clickable: true,
+    },
+    /*Parametros*/
+    speed: 1500,
+    effect: 'fade',
+    grabCursor: true,
+    loop: true,
+    /*Autoplay*/
+    // autoplay: {
+    //   delay: 5000,
+    // },
+    /*keyboard*/
+    keyboard: {
+      enable: true,
+      onlyInViewport: true,
+    }
   });
 
 });
